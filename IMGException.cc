512b74acc1b0f66dd9077e3d2834667a0e9a5548
Michael Torp Kaalund <kaalund@gmail.com>
2018-01-26 21:09:43 +0100

    Renamed IMG2Exception to IMGException

---
+++ IMGException.cc
@@ -0,0 +1,14 @@
+#include "IMGException.h"
+
+IMGException::IMGException(): exception(), msg( SDL_GetError() ) {
+}
+
+IMGException::IMGException( const std::string& _msg ): exception(), msg( _msg ) {
+}
+
+IMGException::~IMGException() throw() {
+}
+
+const char* IMGException::what() const throw() {
+    return msg.c_str();
+}
