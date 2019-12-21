d4d249c3c5d9301dac2346ba4d3e1d98e0f21b87
Michael Torp Kaalund <kaalund@gmail.com>
2019-12-21 11:42:35 +0100

    SDL2mixer has a bug, where Mix_OpenAudio has to be initialize before Mix_Init. So that has now been implemented into MIX class

--- sdl2core_mix/include/sdl2core/MIX.h
+++ sdl2core_mix/include/sdl2core/MIX.h
@@ -12,6 +12,10 @@ public:
     //! Constructor. Basicly runs Mix_Init. This will throw an exception with the Mix error message.
     //! \sa Mix_Init
     //! \sa Mix_GetError
+    //! \param frequency
+    //! \param format
+    //! \param channels
+    //! \param chunksize
     //! \param flags
     //!< Flags can just be or it can be or'ed together
     //!<    -   MIX_INIT_FLAC       -   FLAC audio
@@ -19,7 +23,7 @@ public:
     //!<    -   MIX_INIT_MP3        -   MP3 audio
     //!<    -   MIX_INIT_OGG        -   Vogorbis audio
     //!<
-    MIX( int flags = 0 );// throw( MIXException );
+    MIX( int frequency = 0, Uint16 format = 0, int channels = 0, int chunksize = 0, int flags = 0 );// throw( MIXException );
     //! Destructor.  Basicly runs Mix_Quit.
     //! \sa Mix_Quit
     virtual ~MIX();
--- sdl2core_mix/src/MIX.cc
+++ sdl2core_mix/src/MIX.cc
@@ -1,7 +1,14 @@
 #include "sdl2core/MIX.h"
 
-MIX::MIX( int flag ) {
-    if( ( Mix_Init( flag ) & flag ) != flag ) {
+MIX::MIX( int frequency, Uint16 format, int channels, int chunksize, int flag ) {
+    // There is a bug in SDL2 Mixer, where no support for audio files unless
+    // Mix_OpenAudio is called. So here we are.
+    if( Mix_OpenAudio( frequency, format, channels, chunksize ) < 0 ) {
+        throw MIXException();
+    }
+
+    int result  = 0;
+    if( flag != ( result = Mix_Init( flag ) ) ) {
         throw MIXException();
     }
 }
